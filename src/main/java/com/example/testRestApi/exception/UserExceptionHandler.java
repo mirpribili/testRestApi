468da5f0c1b881c160f6e9f3d662c8a7e8860188
mirpribili <mirpribili@ya.ru>
2023-09-25 17:49:46 +0300

    add UserExceptionHandler

---
+++ src/main/java/com/example/testRestApi/exception/UserExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.testRestApi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class UserExceptionHandler {
+    @ExceptionHandler(value = {UserNotFoundException.class})
+    public UserException handleUserNotFoundException(UserNotFoundException userNotFoundException){
+        UserException userException = new UserException(
+                userNotFoundException.getMessage(),
+                userNotFoundException.getCause(),
+                HttpStatus.NOT_FOUND
+        );
+        return userException;
+    }
+}
